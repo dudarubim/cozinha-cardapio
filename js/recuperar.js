5eaf1bee5a9e06eb72d318bb9d6d0e17efe1b58c
dudarubim <maria.9308820@aluno.mg.gov.br>
2025-10-31 14:11:05 -0300

    ploud

--- js/recuperar.js
+++ js/recuperar.js
@@ -3,4 +3,26 @@ import { recuperarSenha } from "./api.js";
 
 document.getElementById('formRecuperar').addEventListener('submit',async(event)=>{
     
+     event.preventDefault;
+
+    const email=document.getElementById('email').valuetrim();
+
+    if(!email){
+        mostrarMsg('Por favor,verifique email.',red);
+        return
+    }
+    const botao=document.getElementById('recuperar')
+    botao.disabled=true;
+    botao.textContent='Enviando...';
+    const{sucesso,msg,user}=await recuperarSenha(email);
+
+    botao.disabled=false;
+    botao.textContent='Recuperar Senha';
+
+    if(sucesso){
+        mostrarMsg(msg||`Instruções de recuperação enviadas para seu email,${user.name}`,green);
+    }else{
+        mostrarMsg(msg||'Nã foi possivel enviar email de recuperação',red);
+
+    }
 })
